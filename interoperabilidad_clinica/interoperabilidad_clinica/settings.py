87758a38c89fcc7d748f42bb7e0182df3b1f403d
jgarteag <juan.guerrero@correo.tdea.edu.co>
2024-05-06 21:05:07 -0500

    chore: Add initial project files and configurations

--- interoperabilidad_clinica/interoperabilidad_clinica/settings.py
+++ interoperabilidad_clinica/interoperabilidad_clinica/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "clinica"
 ]
 
 MIDDLEWARE = [
@@ -74,9 +75,12 @@ WSGI_APPLICATION = "interoperabilidad_clinica.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'interoperability',
+        'USER': 'root',
+        'PASSWORD': 'juanma',
+        'PORT': '3306',
     }
 }
 
@@ -103,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "es-co"
 
 TIME_ZONE = "UTC"
 

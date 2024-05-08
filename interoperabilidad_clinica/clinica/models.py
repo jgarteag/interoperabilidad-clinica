bd878d5954c7bcb23f2f11d7c5236fc95723d5e8
jgarteag <juan.guerrero@correo.tdea.edu.co>
2024-05-07 20:13:40 -0500

    feat: Add Person model to clinica app

--- interoperabilidad_clinica/clinica/models.py
+++ interoperabilidad_clinica/clinica/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    id_history = models.IntegerField(primary_key=True)
+    country_origin = models.CharField(max_length=200, min_length=3,blank=False, null=False)
+    doc_type = models.CharField(max_length=2, min_length=2, blank=False, null=False)
+    number_doc = models.IntegerField(max_length=20, min_length=3, blank=False, null=False)
+    last_name = models.CharField(max_length=60, min_length=2, blank=False, null=False)
+    surname = models.CharField(max_length=60, min_length=2, blank=False, null=False)
+    first_name = models.CharField(max_length=60, min_length=2, blank=False, null=False)
+    middle_name = models.CharField(max_length=60, min_length=2, blank=False, null=False)
+    date_born = models.DateField(max_length=16, min_length=10, blank=False, null=False)
+    biologic_sex = models.CharField(max_length=2, min_length=2, blank=False, null=False)
+    gender_identity = models.CharField(max_length=2, min_length=2, blank=False, null=False)
+    occupation_care = models.CharField(max_length=4, min_length=4, blank=False, null=False)
+    opossition_donation = models.CharField(max_length=2, min_length=2, blank=False, null=False)
+    date_opossition = models.DateField(max_length=10, min_length=10, blank=False, null=False)
+    antiquated_will_document = models.CharField(max_length=2, min_length=2, blank=False, null=False)
+    date_suscrip_ant_will_doc = models.DateField(max_length=10, min_length=10, blank=False, null=False)
+    cod_borrower = models.CharField(max_length=12, min_length=12, blank=False, null=False)
+    category_disability = models.CharField(max_length=2, min_length=2, blank=False, null=False)
+    habitual_residence = models.CharField(max_length=3, min_length=3, blank=False, null=False)
+    municipality_of_hab_res = models.CharField(max_length=5, min_length=5, blank=False, null=False)
+    ethnicity = models.CharField(max_length=2, min_length=2, blank=False, null=False)
+    territorial_zone = models.CharField(max_length=2, min_length=2, blank=False, null=False)
+    eps = models.CharField(max_length=6, min_length=6, blank=False, null=False)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name + " " + self.surname + " " + self.middle_name 

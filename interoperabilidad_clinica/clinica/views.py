bf778094d8af8fabc4e3509d3c30e1611eae5725
jgarteag <juan.guerrero@correo.tdea.edu.co>
2024-05-10 15:07:15 -0500

    Revert "feat: Add footer to layout and update URLs in clinica app"
    This reverts commit e74dc3783ae2a07b9fbcc45c0e24aaaf73d9f06b.

--- interoperabilidad_clinica/clinica/views.py
+++ interoperabilidad_clinica/clinica/views.py
@@ -1,13 +1,3 @@
 from django.shortcuts import render
-from .models import Countries
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html', {})
-
-def pacient(request):
-    return render(request, 'pacient.html', {})
-
-def countries(request):
-    countries = Countries.objects.all()
-    return render(request, 'pacient.html', {'countries': countries})
